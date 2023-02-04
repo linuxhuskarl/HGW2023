ade6288f484e8e238cd77cdf207eff343f441073
Szymon <grafiks11@gmail.com>
2023-02-04 14:46:07 +0100

    Uzupełnienie poprzedniego commita

--- Assets/Szymon/PlayPiano.cs
+++ Assets/Szymon/PlayPiano.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
+
 
 public class PlayPiano : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip C_sound;
+    public AudioClip D_sound;
+    public AudioClip Dfis_sound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source.volume = 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            source.PlayOneShot(C_sound);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            source.PlayOneShot(D_sound);
+        }
+
     }
 }

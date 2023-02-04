dc69c9aa64bff7749712fe2d7f5cde949ea6bee0
Maciej Brzeski <linuxhuskarl@gmail.com>
2023-02-04 16:04:37 +0100

    Rozwijanie birba, juz mnie sledzi

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+    enum BirdState
+    {
+        Idle,
+        Attack,
+    }
+
+    public float idleSpeed;
+    public float attackSpeed;
+    public Transform player;
+    BirdState state;
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = BirdState.Idle;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 target = new Vector2(player.position.x-1, player.position.y+1);
+        float currentSpeed = GetSpeed(state);
+        transform.position = Vector2.MoveTowards(transform.position, target, currentSpeed * Time.fixedDeltaTime);
+    }
+
+    private float GetSpeed(BirdState state)
+    {
+        switch(state)
+        {
+            case BirdState.Idle:
+                return idleSpeed;
+            case BirdState.Attack:
+                return attackSpeed;
+            default:
+                return 0;
+        }
     }
 
     // Update is called once per frame

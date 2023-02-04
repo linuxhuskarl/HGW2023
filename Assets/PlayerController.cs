17ce1fa37aca9631a0dc84340e46ced8ee6f2f70
Maciej Brzeski <linuxhuskarl@gmail.com>
2023-02-04 16:16:14 +0100

    Dodano złoczyńcę

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        moveInput = Input.GetAxis("Horizontal");
         Jump();
     }
 
